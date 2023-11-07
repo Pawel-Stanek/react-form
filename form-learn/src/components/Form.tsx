f051f1e9e3e9df95392b52c820233a7aea7b7648
Pawel Stanek <pawel.ryszard.stanek@gmail.com>
2023-11-07 20:54:19 +0100

    Pierwszy commit

---
+++ form-learn/src/components/Form.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const form = () => {
+  return (
+    <form>
+    <div className='mb-3'>
+        <label htmlFor='name' className='form-label'>Name</label>
+        <input id='name' type='text' className='form-control' />
+    </div>
+    <div className='mb-3'>
+        <label htmlFor='age' className='form-label'>Age</label>
+        <input id='age' type='number' className='form-control' />
+    </div>
+</form>
+  )
+}
+
+export default form
